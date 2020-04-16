c5edb4b3e9b8d3e7103e14a350bea5c00aef49e8
divyathy <divyathy.eaty11@gmail.com>
2020-04-16 10:41:24 -0700

    TestScripts

--- RestAPIs/src/main/java/Common/BaseURI.java
+++ RestAPIs/src/main/java/Common/BaseURI.java
@@ -1,7 +1,11 @@
 package Common;
 
 public enum BaseURI {
-    sampleURI("http://dummy.restapiexample.com/api/v1/employees");
+    sampleURI("http://dummy.restapiexample.com/api/v1/employees"),
+    employeeURI("http://dummy.restapiexample.com/api/v1/employee/1"),
+    createEmployeeID("http://dummy.restapiexample.com/api/v1/create"),
+    updateEmployeeID("http://dummy.restapiexample.com/api/v1/update/21"),
+    deleteEmployeeID("http://dummy.restapiexample.com/api/v1/delete/2");
 
     private final String baseURI;
     BaseURI(String baseURI) {this.baseURI=baseURI;}
--- RestAPIs/src/main/java/RestAssuredAPIs/RestAPIs.java
+++ RestAPIs/src/main/java/RestAssuredAPIs/RestAPIs.java
@@ -7,9 +7,10 @@ import io.restassured.response.Response;
 
 import java.util.Map;
 
-import static Common.BaseURI.sampleURI;
+import static Common.BaseURI.*;
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
+import static org.apache.http.HttpStatus.SC_BAD_REQUEST;
 import static org.apache.http.HttpStatus.SC_OK;
 
 public class RestAPIs {
@@ -18,10 +19,9 @@ public class RestAPIs {
     public static void setBaseUri() {
         RestAssured.baseURI = sampleURI.getBaseURI();
     }
-
     @Test
-    public void testSeleniumScripts(){
-
+    public void testEmployeeAPI(){
+        RestAssured.baseURI = sampleURI.getBaseURI();
         Response response=
                 given().log().everything().
                         when().
@@ -30,5 +30,52 @@ public class RestAPIs {
         Map<String, Object> actualResult = response.jsonPath().get("$");
         System.out.println(actualResult);
 
+    }
+    @Test
+    public void testEmployeeIdAPI(){
+        RestAssured.baseURI = employeeURI.getBaseURI();
+        Response response =
+                given().log().everything().
+                        when().
+                        get(baseURI).
+                        then().assertThat().statusCode(SC_BAD_REQUEST).extract().response();
+        Map<String, Object> actualResult= response.jsonPath().get("$");
+        System.out.println(actualResult);
+    }
+
+    @Test
+    public void testCreateEmpID(){
+        baseURI= createEmployeeID.getBaseURI();
+        Response response=
+                given().log().everything().
+                        when().
+                        post(baseURI).
+                        then().assertThat().extract().response();
+        Map<String, Object> actualResult= response.jsonPath().get("$");
+        System.out.println(actualResult);
+    }
+
+    @Test
+    public void testUpdateEmpID(){
+        baseURI=updateEmployeeID.getBaseURI();
+        Response response=
+                given().log().everything().
+                        when()
+                .put(baseURI)
+                .then().assertThat().extract().response();
+        Map<String, Object> actualResult= response.jsonPath().get("$");
+        System.out.println(actualResult);
+    }
+
+    @Test
+    public void testDeleteEmpID(){
+        baseURI=deleteEmployeeID.getBaseURI();
+        Response response=
+                given().log().everything()
+                .when()
+                .delete(baseURI)
+                .then().assertThat().extract().response();
+        Map<String, Object> actualResult= response.jsonPath().get("$");
+        System.out.println(actualResult);
     }
 }
